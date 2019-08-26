5ba8b8b2374cf8a85f4fabc943d571719844399b
JKorum <constantine.proshin@gmail.com>
2019-08-26 13:00:59 +0300

    add patch and delete routes

--- src/index.js
+++ src/index.js
@@ -99,4 +99,93 @@ app.get(`/tasks/:id`, async (req, res) => {
 	}	
 });
 
+app.patch(`/users/:id`, async (req, res) => { //properties is req.body that don't exist in UserSchema are ignored
+	const updates = Object.keys(req.body); 
+	const allowedToUpdate = [`name`, `email`, `password`, `age`];
+	const isValidOperation = updates.every(update => allowedToUpdate.includes(update));
+	if(!isValidOperation) {
+		return res.status(400).send({ error: `invalid field names provided` }); //unify error sending objects' structure
+	}
+
+	try { //mongoose automatically converts string id --> ObjectID
+		const queryOptions = {
+			new: true, //return the updated document rather then the original 
+			runValidators: true //run the schema validators against the updating object
+		};
+		const updatedUser = await UserModel.findByIdAndUpdate(req.params.id, req.body, queryOptions);
+		if(!updatedUser) { //if no user with a correctly formated id --> updatedUser === null
+			return res.status(404).send();
+		}
+		res.status(200).send(updatedUser);
+
+	} catch(error) {
+		if(error.name === `CastError`) { //`findById` throws an error if an id is poorly formated
+			return res.status(400).send({
+				status: `failed to fetch document`,
+				error: `invalid id provided`
+			});
+		}
+		res.status(400).send(error); //for validation errors --> refactor sending data
+	}
+});
+
+app.patch(`/tasks/:id`, async (req, res) => {
+	const updates = Object.keys(req.body); //consider to refactor to a function --> ./utilities/smth.js OR middleware?
+	const allowedToUpdate = [`description`, `status`];
+	const isValidOperation = updates.every(update => allowedToUpdate.includes(update));
+	if(!isValidOperation) {
+		return res.status(400).send({ error: `invalid field names` });
+	}
+
+	try { //mongoose automatically converts string id --> ObjectID
+		const queryOptions = {
+			new: true, //return the updated document rather then the original 
+			runValidators: true //run the schema validators against the updating object
+		};
+		const updatedTask = await TaskModel.findByIdAndUpdate(req.params.id, req.body, queryOptions);
+		if(!updatedTask) { //if no task with a correctly formated id --> updatedTask === null
+			return res.status(404).send();
+		}
+		res.status(200).send(updatedTask);
+
+	} catch(error) {
+		if(error.name === `CastError`) { //`findById` throws an error if an id is poorly formated
+			return res.status(400).send({ error: `invalid value for id field` });
+		}
+		res.status(400).send(error); //for validation errors --> refactor sending data
+	}
+});
+
+app.delete(`/users/:id`, async (req, res) => {
+	try {
+		const deletedUser = await UserModel.findByIdAndDelete(req.params.id);
+		if(!deletedUser) { //if no user with a correctly formated id --> deletedUser === null
+			return res.status(404).send();
+		}
+		res.status(200).send(deletedUser);
+
+	} catch(error) {
+		if(error.name === `CastError`) { //`findById` throws an error if an id is poorly formated
+			return res.status(400).send({ error: `invalid value for id field` }); // set up a middleware or a utility
+		}
+		res.status(500).send();
+	}		
+});
+
+app.delete(`/tasks/:id`, async (req, res) => { 
+	try {
+		const deletedTask = await TaskModel.findByIdAndDelete(req.params.id);
+		if(!deletedTask) { //if no task with a correctly formated id --> deletedTask === null
+			return res.status(404).send();
+		}
+		res.status(200).send(deletedTask);
+
+	} catch(error) {
+		if(error.name === `CastError`) { //`findById` throws an error if an id is poorly formated
+			return res.status(400).send({ error: `invalid value for id field` }); // set up a middleware or a utility
+		}
+		res.status(500).send();
+	}
+});
+
 app.listen(port, () => console.log(`server is listening on port ${port}`));
