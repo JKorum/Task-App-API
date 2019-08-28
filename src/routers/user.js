3c985f6acbae4c9ed5902f3889fbce4f0224a1e0
JKorum <constantine.proshin@gmail.com>
2019-08-28 21:10:22 +0300

    delete route: get user/:id

--- src/routers/user.js
+++ src/routers/user.js
@@ -61,24 +61,6 @@ router.get(`/users/me`, auth, async (req, res) => {
 })
 
 
-router.get(`/users/:id`, async (req, res) => {
-	const _id = req.params.id;
-	try { //mongoose automatically converts string id --> ObjectID
-		const user = await UserModel.findById(_id);
-		if(!user){ //if no user with such id --> user === null
-			return res.status(404).send();
-		}
-		res.status(200).send(user);
-	} catch(error) {
-		if(error.name === `CastError`) { //`findById` throws an error if an id is poorly formated
-			return res.status(400).send({
-				status: `failed to fetch document`,
-				error: `invalid id provided`
-			});
-		}		
-		res.status(500).send();
-	}	
-});
 
 router.patch(`/users/:id`, async (req, res) => { //properties is req.body that don't exist in UserSchema are ignored
 	const updates = Object.keys(req.body); 
